62e3f44411eba66b70122d4231f8a00704777622
RomeoIslamRasel <98464358+RomeoIslamRasel@users.noreply.github.com>
2024-04-05 06:04:56 +0600

    Update ping.js

--- scripts/cmds/ping.js
+++ scripts/cmds/ping.js
@@ -1,25 +1,44 @@
 module.exports = {
-  config: {
-    name: "ping",
-    aliases: ["ms"],
-    version: "1.0",
-    author: "Sandu",
-    role: 0,
-    shortDescription: {
-      en: "Displays the current ping of the bot's system."
-    },
-    longDescription: {
-      en: "Displays the current ping of the bot's system."
-    },
-    category: "System",
-    guide: {
-      en: "Use {p}ping to check the current ping of the bot's system."
+      config: {
+         name: "ping",
+         author: "UPoL",
+         role: 0,
+         version: "2.9",
+         shortDescription: "Check ping status",
+         longDescription: "Check net speed",
+         guide: {
+            en: "{pn}"
+        }
+   },
+   onStart: async function ({ api, event, message }) {
+         const timeStart = Date.now();
+         await message.reply("loding the net speed browser..");
+         const ping = Date.now() - timeStart;
+         let pingStatus = " 🟢 | Very Good ";
+    if (ping > 200) {
+      pingStatus = " 🌸 | Good..";
     }
-  },
-  onStart: async function ({ api, event, args }) {
-    const timeStart = Date.now();
-    await api.sendMessage("Checking Bot's ping", event.threadID);
-    const ping = Date.now() - timeStart;
-    api.sendMessage(`The current ping is ${ping}ms.`, event.threadID);
-  }
-};
+    if (ping > 500) {
+      pingStatus = " ✅ | Medium..!!";
+    }
+    if (ping > 800) {
+      pingStatus = " ⚠ | Not Good-";
+    }
+    if (ping > 1000) {
+      pingStatus = " 👀 | Net slow.....";
+    }
+    if (ping > 1200) {
+      pingStatus = " 🚫 | Oho Net Issue.";
+    }
+    if (ping > 1500) {
+      pingStatus = " ⚠ | Bad.!";
+    }
+    if (ping > 1800) {
+      pingStatus = " ❌ | Very Bad..";
+    }
+    if (ping > 2000) {
+      pingStatus = " 💀 | Fully Dead.";
+    }
+         message.reply(`———|Status|———\n😊 Your browser ping :${ping}ms.\nStatus: ${pingStatus}`);
+       } 
+  };
